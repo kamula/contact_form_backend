a32b8b8c693f959a6e8711f0919617f1537229bf
kamula <kamulaisaac@gmail.com>
2024-05-04 15:12:45 +0300

    validate post request data to ensure that all fields are required

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Contact;
+
+class ContactController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // Request validation(Required fields)
+        $request->validate([
+            'fullName' => 'required',
+            'email' => 'required',
+            'message' => 'required',
+        ]);
+        //Create products
+        return Contact::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
